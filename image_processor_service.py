00b20b763d0487e181fe7d3f8647494bf0949a92
Daniela Cordoba <dacorac43@gmail.com>
2024-08-30 14:48:49 +1000

    REST Service and functions to remove image background and overlay image with background

--- image_processor_service.py
+++ image_processor_service.py
@@ -1,8 +1,27 @@
-import pixellib
-from pixellib.tune_bg import alter_bg
+from rembg import remove
+from PIL import Image
+import base64
 
-change_bg = alter_bg(model_type = "pb")
-change_bg.load_pascalvoc_model("xception_pascalvoc.pb")
-change_bg.change_bg_img(f_image_path = "sample.jpg", b_image_path = "background.jpg", output_image_name="new_img.jpg")
+def remove_background(input_path, filename):
+  output_path = f"{filename}_output.png"
+  input = Image.open(input_path)
+  output = remove(input)
+  output.save(output_path)
 
+  return output_path
 
+def overlay_images(input_path, background_id):
+  input = Image.open(input_path)
+  # change when background images path are defined
+  # background = Image.open(background_id)
+  background = Image.open('background.jpg')
+  background.paste(input, (0,0), mask = input) 
+  
+  # Saves the output image locally
+  final_path = "output.png"
+  background.save(final_path)
+
+  # Base64 encode to return image to client
+  with open(final_path, 'rb') as image_file:
+    base64_bytes_image = base64.b64encode(image_file.read())
+  return base64_bytes_image
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import datetime
 import os
 import gmail_api_service
+import image_processor_service
 
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
+dir_actual = os.path.dirname(os.path.abspath(__file__))
 
 # Ensure the upload folder exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
@@ -13,6 +16,29 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 @app.route('/')
 def index():
   return "Hello World!"
+
+@app.route('/transform_image', methods=['POST'])
+def transform_image():
+  if 'image' not in request.files:
+    return jsonify({'error': 'No image uploaded'}), 400
+  
+  # Get the image file from the request
+  image_file = request.files['image']
+  now = datetime.datetime.now()
+  filename = f"{now.strftime('%Y-%m-%d %H-%M-%S')}-{image_file.filename}"
+  image_file.save(os.path.join(dir_actual, filename))
+    
+  removed_background_path = image_processor_service.remove_background(os.path.join(dir_actual, filename), filename)
+
+  background_choice = request.form['background_id']
+  try:
+    response = image_processor_service.overlay_images(removed_background_path, background_choice)
+    os.remove(os.path.join(dir_actual, filename))
+    return response
+  except Exception as e:
+    print(f"Error processing your image: {e}")
+    return jsonify({'error': 'Error processing your image'}), 500
+
 
 @app.route('/send_email', methods=['POST'])
 def send_email():
